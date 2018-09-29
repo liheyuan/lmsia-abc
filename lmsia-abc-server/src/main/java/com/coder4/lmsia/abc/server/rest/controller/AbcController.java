605d01fca57fa5b9df189ce0b9cab6585a7a87f6
coder4 <lihy@fenbi.com>
2018-09-29 16:06:31 +0800

    FIX: add rate limit

--- lmsia-abc-server/src/main/java/com/coder4/lmsia/abc/server/rest/controller/AbcController.java
+++ lmsia-abc-server/src/main/java/com/coder4/lmsia/abc/server/rest/controller/AbcController.java
@@ -4,10 +4,13 @@ import com.coder4.lmsia.abc.constant.LmsiaAbcConstant;
 import com.coder4.lmsia.abc.server.configuration.TestConfig;
 import com.coder4.lmsia.abc.server.rest.logic.intf.AbcLogic;
 import com.coder4.lmsia.hystrix.BaseHystrixCommend;
+import com.coder4.lmsia.ratelimit.MethodParamRateLimit;
+import com.coder4.lmsia.ratelimit.MethodRateLimit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,13 @@ public class AbcController {
     @Autowired
     private TestConfig testConfig;
 
+    @MethodParamRateLimit(permitsPerSecond = 1, paramIndex = 0)
+    @GetMapping(value = "/ids/{id}")
+    public String helloWithId(@PathVariable int id) {
+        return helloFallback();
+    }
+
+    @MethodRateLimit(permitsPerSecond = 2.0)
     @GetMapping(value = "/")
     public String hello() {
         return new BaseHystrixCommend<String>("abc", this::helloReal, this::helloFallback).execute();
@@ -40,7 +50,7 @@ public class AbcController {
         return abcLogic.getHello();
     }
 
-    private String helloFallback() {
+    public String helloFallback() {
         LOG.info("hello fb");
         return "Hello, fallback";
     }
